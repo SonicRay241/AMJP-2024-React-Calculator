9ccaf38cee14a5f53e27333860356b838f1d96d3
SonicRay241 <rayhan.permana@yahoo.com>
2023-11-18 19:10:50 +0700

    completed calculator, next step: support page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useRouter } from 'next/navigation';
 import { useState } from "react"
 
 const Home = () => {
@@ -10,25 +11,58 @@ const Home = () => {
   )
 }
 
+const numbers = "1234567890"
+const operations = "+-x/"
+type TCalcOperations = "+" | "-" | "x" | "/" | null
+type TCalcState = "leftNum" | "rightNum" | "operation" | "result"
+type TButtonColor = "gray" | "orange" | "brown"
+
 type TCalcButtonParam = {
   value: string
   callback: () => void
+  width: number
+  color: TButtonColor
 }
 
-const numbers = "1234567890"
-
-type TCalcOperations = "+" | "-" | "*" | "/" | null
-type TCalcState = "leftNum" | "rightNum" | "operation" | "result"
+let numberLeft: number = 0, numberRight: number, operation: TCalcOperations, result: string
 
 const Calculator = () => {
+  const { push } = useRouter()
+
   const [displayValue, setDisplayValue] = useState("0")
-  const [history, setHistory] = useState<number[]>([])
+  const [history, setHistory] = useState<string[]>([])
+  const [calculatorState, setCalculatorState] = useState<TCalcState>("leftNum")
 
-  let numberLeft: number, numberRight: number, operation: TCalcOperations, result: number
-  let calculatorState: TCalcState = "leftNum"
+  const createButton = (value: string, width: number = 1, color: TButtonColor = "gray") => {
+    return { value: value, callback: () => { processCalcLogic(value) }, width: width, color }
+  }
 
-  const createButton: (value: string) => TCalcButtonParam = (value: string) => {
-    return { value: value, callback: () => { processCalcLogic(value) } }
+  const calculate: () => string = () => {
+    let result: string
+    switch (operation) {
+      case "+": 
+        result = (+(numberLeft + numberRight)).toString()
+        break
+
+      case "-": 
+        result = (+(numberLeft - numberRight)).toString()
+        break
+
+      case "x": 
+        result = (+(numberLeft * numberRight)).toString()
+        break
+
+      case "/": 
+        result = (+(numberLeft / numberRight)).toString()
+        break
+
+        
+
+      default:
+        return "Err"
+    }
+    if (result == "Infinity" || result == "NaN") return "Err"
+    return result
   }
 
   const processCalcLogic = (buttonValue: string) => {
@@ -38,36 +72,100 @@ const Calculator = () => {
           setHistory([...history, result])
         }
         setDisplayValue("0")
-        calculatorState = "leftNum"
-        break;
+        // calculatorState = "leftNum"
+        setCalculatorState("leftNum")
+        break
+
       case "DEL":
-        setDisplayValue(displayValue.replace(/.$/, ""))
-        if (displayValue == "") setDisplayValue("0")
-        break;
+        if (calculatorState == "result") {
+          setHistory([...history, result])
+          setCalculatorState("leftNum")
+        }
+        const display = displayValue.replace(/.$/, "")
+        setDisplayValue(display == "" ? "0" : display)
+        break
+
+      case "=":
+        if (calculatorState == "rightNum") {
+          result = calculate()
+          setCalculatorState("result")
+          setDisplayValue(result)
+        }
+        break
+
+      case "?":
+        push("/support")
+        break
+
       default:
         if (numbers.includes(buttonValue)) {
-          setDisplayValue((+(displayValue+buttonValue)).toString())
+          if (calculatorState == "result") {
+            setHistory([...history, result])
+            setDisplayValue(buttonValue)
+            numberLeft = +buttonValue
+            setCalculatorState("leftNum")
+          }
+          if (calculatorState == "leftNum") {
+            numberLeft = +(displayValue+buttonValue)
+            numberRight = +(displayValue+buttonValue)
+            setDisplayValue(numberLeft.toString())
+          }
+          if (calculatorState == "operation") setCalculatorState("rightNum")
+
+          if (operations.includes(displayValue[0]) && calculatorState == "operation") {
+            numberRight = +buttonValue
+            setDisplayValue(numberRight.toString())
+          } else {
+            numberRight = +(displayValue+buttonValue)
+            setDisplayValue(numberRight.toString())
+          }
+          
         }
-        break;
+        if (operations.includes(buttonValue)) {
+          if (calculatorState == "leftNum") setCalculatorState("operation")
+          // if calculatorstate == rightnum, calculate first and move the result to firstnum then return to operation state
+          if (calculatorState == "rightNum" || calculatorState == "result") {
+            let calculation = calculate()
+            if (calculation == "Err") {
+              numberLeft = 0
+            } else numberLeft = +calculation
+            if (calculatorState == "result") setHistory([...history, calculation])
+            setCalculatorState("operation")
+          }
+          operation = buttonValue as TCalcOperations
+          setDisplayValue(buttonValue)
+        }
+
+        break
     }
+
+    // console.log(numberLeft);
+    // console.log(numberRight);
+    // console.log(operation);
+    
   }
 
   const buttons: string[] = ["C", "DEL", "?", "/", "1", "2", "3", "x", "4", "5", "6", "-", "7", "8", "9", "+", "0", "="]
 
   return (
-    <div>
-      {
-        history.map((value, _) => {
-          return <h1>{value}</h1>
-        })
-      }
-      <h1>{displayValue}</h1>
-      
-      <div className="grid grid-cols-4 grid-rows-4 gap-4">
+    <div className="rounded-3xl pb-4">
+      <div className="bg-zinc-600 h-30 rounded-t-3xl">
+        <div className="h-20 pt-4 px-4 text-2xl overflow-auto no-scrollbar">
+          {
+            history.map((value, _) => {
+              return <h1>{value}</h1>
+            })
+          }
+        </div>
+        
+        {/* <h1>{calculatorState}</h1> */}
+        <h1 className="text-end text-6xl px-4 pb-2">{displayValue}</h1>
+      </div>
+      <div className="grid grid-cols-4 gap-4 relative w-80 h-96 bg-black p-4 rounded-b-3xl">
         {
-          buttons.map((value, _) => {
-            const button: TCalcButtonParam = createButton(value)
-            return <CalcButtonComponent value={button.value} callback={button.callback}/>
+          buttons.map((value, idx) => {
+            const button: TCalcButtonParam = createButton(value, idx < 16 ? 1 : 2, ((idx+1) % 4 == 0 || idx == 17) ? "orange" : idx == 2 ? "brown" : "gray")
+            return <CalcButtonComponent value={button.value} callback={button.callback} width={button.width} color={button.color}/>
           })
         }
       </div>
@@ -76,11 +174,19 @@ const Calculator = () => {
 }
 
 
-const CalcButtonComponent = ({ value, callback }: TCalcButtonParam) => {
+const CalcButtonComponent = ({ value, callback, width, color }: TCalcButtonParam) => {
+  let elementWidth = `col-span-1`
+  if (width == 2) {
+    elementWidth = `col-span-2`
+  }
+  let btnColor: string
+  switch (color) {
+    case "gray": btnColor = "bg-gray-500"; break
+    case "brown": btnColor = "bg-[#866242]"; break
+    case "orange": btnColor = "bg-amber-500"; break
+  }
   return (
-    <div>
-      <button onClick={callback}>{value}</button>
-    </div>
+      <button className={`${elementWidth} rounded-full text-xl ${btnColor}`} onClick={callback}>{value}</button>
   )
 }
 
